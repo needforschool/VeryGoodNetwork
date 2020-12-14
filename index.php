6dfa8d27617c3a446ebd0c58869d4ca6e3b4d036
QVan27 <qvannarath@gmail.com>
2020-12-14 17:36:39 +0100

    ajout btn

--- index.php
+++ index.php
@@ -15,21 +15,27 @@ include('inc/header.php');
                 <h1>VeryGoodNetwork</h1>
                 <p>Responsive Network Website available now 24h/24h</p>
         </div>
+        <div class="banner-btn">
+                <?php if (empty($_SESSION)) : ?>
+                        <a href="signin.php" class="btn btn1">Inscription</a>
+                        <a href="login.php" class="btn btn2">Connexion</a>
+                <?php endif; ?>
+        </div>
 </section>
 
 
-                <div class="wrap">  
-                 <div class="content">
-                  <h2>Lorem ipsum dolor s<br />Lorem ipsum dolor sit amet consectetur, .<br />Lorem ipsum dolor sit amet consectetur, adipisicing .</h2>
-                </div>
-               </div>
-               <div class="content2">
-               <img src="img_avatar.png" alt="Avatar" class="image" style="width:100%">
-  <div class="middle">
-    <div class="text">John Doe</div>
-  </div>
+<div class="wrap">
+        <div class="content">
+                <h2>Lorem ipsum dolor s<br />Lorem ipsum dolor sit amet consectetur, .<br />Lorem ipsum dolor sit amet consectetur, adipisicing .</h2>
+        </div>
+</div>
+<div class="content2">
+        <img src="img_avatar.png" alt="Avatar" class="image" style="width:100%">
+        <div class="middle">
+                <div class="text">John Doe</div>
+        </div>
 
-               </div>
+</div>
 
 <?php
 include('inc/footer.php');
