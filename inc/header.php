e807330c4f5bf0a89ff5518e30d7da6fe606f079
QVan27 <qvannarath@gmail.com>
2020-12-14 17:07:21 +0100

    conflit

--- inc/header.php
+++ inc/header.php
@@ -8,7 +8,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
         <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="asset/css/jquery.fatNav.min.css">
+        <link rel="stylesheet" href="asset/css/jquery.fatNav.css">
         <!--Style micromodal-->
         <link rel="stylesheet" href="asset/css/micromodal-style.css">
         <!--Style Perso-->
