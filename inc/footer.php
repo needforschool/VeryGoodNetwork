02595c405b6d466755704d123d2c20b28a5ace11
QVan27 <qvannarath@gmail.com>
2020-12-14 14:53:00 +0100

    ajout nav

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,8 @@
         <footer>
         <!--JQuery-->
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="asset/js/jquery.fatNav.min.js"></script>
+
         <!--Chart.js-->
         <!--JS Perso-->
         <script src="asset/js/main.js" type="text/javascript"></script>
