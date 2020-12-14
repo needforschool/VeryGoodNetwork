8931bae7753430fbb59e90f0baa290145912b1fa
Oliebat <bationo.c@gmail.com>
2020-12-14 16:49:24 +0100

    middlehomepage

--- index.php
+++ index.php
@@ -7,8 +7,21 @@ $title = 'Home';
 
 include('inc/header.php');
 ?>
-      
+
         <h1>Home</h1>
+
+                <div class="wrap">  
+                 <div class="content">
+                  <h2>Lorem ipsum dolor s<br />Lorem ipsum dolor sit amet consectetur, .<br />Lorem ipsum dolor sit amet consectetur, adipisicing .</h2>
+                </div>
+               </div>
+               <div class="content2">
+               <img src="img_avatar.png" alt="Avatar" class="image" style="width:100%">
+  <div class="middle">
+    <div class="text">John Doe</div>
+  </div>
+
+               </div>
 
 <?php
 include('inc/footer.php');
