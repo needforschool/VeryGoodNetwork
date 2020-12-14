04b0a95b395d7a5a1fcf327c725bd842fd3b1eaa
ImJustLucas <contact@lucasbellier.fr>
2020-12-14 14:44:30 +0100

    add - micromodal

--- inc/footer.php
+++ inc/footer.php
@@ -2,6 +2,8 @@
         <!--JQuery-->
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
         <!--Chart.js-->
+        <!--MicroModal-->
+        <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
         <!--JS Perso-->
         <script src="asset/js/main.js" type="text/javascript"></script>
         </footer>
