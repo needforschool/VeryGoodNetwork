16c3699b8ecf1244e5548094142283808d80a67e
ImJustLucas <contact@lucasbellier.fr>
2020-12-15 14:27:14 +0100

    micromodal = plugin de merde

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,44 +1,38 @@
-//-----------------
-//PLUGIN MICROMODAL
-//-----------------
-
-MicroModal.init({
-    onShow: modal => console.info(`${modal.id} is shown`),
-    onClose: modal => console.info(`${modal.id} is hidden`),
-    openTrigger: 'data-custom-open', 
-    closeTrigger: 'data-custom-close',
-    openClass: 'is-open',
-    disableScroll: true,
-    disableFocus: false, 
-    awaitOpenAnimation: false, 
-    awaitCloseAnimation: false, 
-    debugMode: true
-});
-
-var buttonLogin = $('#openModalLogin');
-buttonLogin.on('click', function(){
-    MicroModal.show('#modal-login');
-});
-
-var buttonSignin = $('#openModalSignin');
-buttonSignin.on('click', function(){
-    MicroModal.show('#modal-signin');
-});
-
-//----------------------
-//PLUGIN MENU NAVIGATION
-//----------------------
-
-(function() {
-    
-    $.fatNav();
-    
-}());
-
 //----------------------
 //OUVERTURE JQUERY
 //----------------------
 
 $(document).ready(function(){
 
+    //-----------------
+    //PLUGIN MICROMODAL
+    //-----------------
+
+    MicroModal.init({
+        onShow: modal => console.info(`${modal.id} is shown`),
+        onClose: modal => console.info(`${modal.id} is hidden`),
+        openTrigger: 'data-custom-open', 
+        closeTrigger: 'data-custom-close',
+        openClass: 'is-open',
+        disableScroll: true,
+        disableFocus: false, 
+        awaitOpenAnimation: false, 
+        awaitCloseAnimation: false, 
+        debugMode: true
+    });
+
+    //----------------------
+    //PLUGIN MENU NAVIGATION
+    //----------------------
+
+    (function() {
+        
+        $.fatNav();
+        
+    }());
+
+
+//----------------------
+//FERMETURE JQUERY
+//----------------------
 })
