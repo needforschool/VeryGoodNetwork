502be61712661ec4db91a12c111032a3fd794228
CesTheo <theo.maudet@icloud.com>
2020-12-15 10:27:37 +0100

    about us 0.1

--- about-us.php
+++ about-us.php
@@ -8,7 +8,95 @@ $title = 'A propos';
 include('inc/header.php');
 ?>
       
-        <h1>A propos</h1>
+        <section>
+                <h1>About US</h1> 
+        </section>
+        
+        <section id="about-section1">
+                <h1 class="title">Nos services</h1>
+                <p>Analyse de votre réseau pour votre entreprise Des solutions innovantes pour vous</p>
+                <img src="asset/img/about-picture1.jpg" alt="">
+        </section>
+
+        <section>
+                <h1>Notre Équipe</h1>
+                <div>
+                        <div>
+                                <img>
+                                <p>Lucas</p>
+                        </div>
+                        <div>
+                                <img>
+                                <p>Lucas</p>   
+                        </div>
+                        <div>
+                        <img>
+                                <img>
+                                <p>Lucas</p>  
+                        </div>
+                        <div>
+                                <img>
+                                <p>Lucas</p>        
+                        </div>
+                        <div>
+                                <img>
+                                <p>Lucas</p>   
+                        </div>
+                </div>
+        </section>
+
+        <section>
+
+        <div>
+                <div>
+                        <p>1<span>er</span></p>
+                        <p>En Normandie</p>
+                </div>
+                <div>
+                        <p>4</p>
+                        <p>Développeurs</p>
+                </div>
+                <div>
+                        <p>56</p>
+                        <p>Partenaires</p>  
+                </div>
+                <div>
+                        <p>6</p>
+                        <p>Module d'analyse</p>      
+                </div>
+        </div>
+        </section>
+
+        <section>
+                <h1>Notre Interface</h1>
+                <div>
+
+                </div>
+                <div>
+                        <div></div>
+                        <div>
+                                <p>Ergonomique</p>
+                                <p>Performantes</p>
+                                <p>Simple à comprendre</p>
+                                <p>Cliquez sur l'image pour tester</p>
+                        </div>
+                </div>
+        </section>
+
+        <section>
+                <h1>Ils parlent de nous</h1>
+                <div>
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                </div>
+        </section>
+
+        <section>
+                <h1>Satisfait ?</h1>
+                <button>Inscrivez vous</button>
+        </section>
+
 
 <?php
 include('inc/footer.php');
