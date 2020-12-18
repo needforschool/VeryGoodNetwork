6f90fb4582a5eacb21970f3cf37d038b4f7d9568
ImJustLucas <contact@lucasbellier.fr>
2020-12-18 15:08:29 +0100

    Ajout des dates + log

--- ajax/ajax-getDataTrame.php
+++ ajax/ajax-getDataTrame.php
@@ -1,3 +1,41 @@
 <?php
 include('../inc/pdo.php');
 include('../inc/function.php');
+$trames = array();
+$i = 0;
+setlocale(LC_ALL,'fr_FR');
+
+    $sql = "SELECT date,version,status,ttl,protocol_name,ip_from,ip_dest FROM vgn_trames";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $dataTrames = $query->fetchAll();
+
+    //var_dump($trames);
+
+    foreach ($dataTrames as $dataTrame) {
+        
+        $trames[$i]['date'] = $dataTrame['date'];
+        $trames[$i]['version'] = $dataTrame['version'];
+        $trames[$i]['status'] = $dataTrame['status'];
+        $trames[$i]['ttl'] = $dataTrame['ttl'];
+        $trames[$i]['protocol_name'] = $dataTrame['protocol_name'];
+        $trames[$i]['ip_from'] = $dataTrame['ip_from'];
+        $trames[$i]['ip_dest'] = $dataTrame['ip_dest'];
+
+
+        //Décryptage des dates
+        $dateLog = date("Y-m-d H:i:s", $dataTrame['date']);
+        $dateGraph = strftime("%A %d %B", $dataTrame['date']);
+        $trames[$i]['date-log'] = $dateLog;
+        $trames[$i]['date-graph'] = $dateGraph;
+
+        //Création des logs
+
+        $logTrame = $trames[$i]['date-log'] . ' - Source (X.X.X.X.) vers (X.X.X.X.) - requête '. $trames[$i]['protocol_name'] .' - OK';
+        $trames[$i]['log'] = $logTrame;
+        //echo $dateLog;
+        //debug($trames);
+        ++$i;
+    }
+
+    debug($trames);
--- inc/function.php
+++ inc/function.php
@@ -119,3 +119,4 @@ function getAllFrom($table, $pdo)
   $data = $query->fetchAll();
   return $data;
 }
+
